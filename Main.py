0051a0c9e8a4f3f7502416a511e3e91fd366c26f
URK21CS1147KUSHMITHA <109952808+URK21CS1147KUSHMITHA@users.noreply.github.com>
2022-08-22 14:40:06 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,144 +1,138 @@
-class MyCircularQueue:
+from typing import Optional
 
-    def _init_(self, size: int):
 
-        self.queue = [0] * size
+class Node:
+ """
+ Provide necessary documentation
+ """
+ def __init__(self, data=None, next=None):
+ """
+ Provide necessary documentation
+ """
+ self.data = data
+ self.next = next
 
-        self.size = size
 
-        self.front, self.rear = -1, -1
+from typing import Optional
 
+
+class Node:
+ """
+ This class describes Node objects to act as elements of the LinkedList
+ Attributes:
+ -> data - stored associated data
+ -> next - link to next node
+ """
+
+ def __init__(self, data=None, next=None):
+ """
+ Initialises the Node with given attributes
+ """
+ self.data = data
+ self.next = next
+
+
+class LinkedList:
+ """
+ This class implements LinkedList using Node objects
+ Methods:
+ -> insert_at_end - inserts node with data at the end of the list
+ -> status - displays all elements of the lisT
+ Attributes
+ -> self.head - contains first node of LinkedList, None if list empty
+ """
+
+ def __init__(self):
+ """
+ Initialize the head
+ """
+ self.head = None
+
+ def insert_at_end(self, data):
+ """
+ Insert node at end of the list
+ :param data: integer data that will be used to create a node
+ """
+ new = Node(data, None)
+ current = self.head
+ if current is None:
+ self.head = new
+ else:
+ while current.next is not None:
+ current = current.next
+ current.next = new
+
+ def status(self):
+ """
+ It prints all the elements of list.
+ """
+ elements = []
+ current = self.head
+ while current:
+ elements.append(current.data)
+ current = current.next
+ print(elements)
+
+
+class Solution:
+ """
+ Class implementing functions to add numbers in a LinkedListT
+ 
+ """
+
+ def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[
+ LinkedList]:
+ """
+ :param first_list: Linkedlist with non-negative integers
+ :param second_list: Linkedlist with non-negative integers
+ :return: returns the sum as a linked list
+ """
+ result = self.get_num(first_list) + self.get_num(second_list)
+ sum_list = LinkedList()
+ for digit in list(map(int, str(result)[::-1])):
+ sum_list.insert_at_end(digit)
+ return sum_list
+
+ def get_num(self, l: Optional[LinkedList]) -> int:
+ """
+ :param l: LinkedList with non-negative integers
+ :return: returns digits of the list as a single integer
+ """
+ curr = l.head
+ if curr is None:
+ return 0
+ num = ""
+ while curr is not None:
+ num = str(curr.data) + num
+ curr = curr.next
+ return int(num)
+
+
+# Do not edit the following code
+# Create an instance for LinkedList
+first_list = LinkedList()
+# Create an another instance for LinkedListT
+second_list = LinkedList()
+# Read data for first list
+data_for_first_list = list(map(int, input().strip().split(" ")))
+# Add data at the end of first_list
+for data in data_for_first_list:
+ first_list.insert_at_end(data)
+# Read data for second list
+data_for_second_list = list(map(int, input().strip().split(" ")))
+# Add data at the end of second_list
+for data in data_for_second_list:
+ second_list.insert_at_end(data)
+# Create an instance for Solution
+solution = Solution()
+# Pass first_list and second_list to addTwoNumbers, which returns a new linked list
+new_list = solution.addTwoNumbers(first_list, second_list)
+# Display the status of new_list
+new_list.status()
+
+
+     
  
 
-    def enqueue(self, value: int) -> bool:
 
-        if self.is_full():
-
-            return False
-
-        if self.front == -1:
-
-            self.front, self.rear = 0, 0
-
-        else:
-
-            self.rear = (self.rear + 1) % self.size
-
-        self.queue[self.rear] = value
-
-        return True
-
- 
-
-    def dequeue(self) -> bool:
-
-        if self.is_empty():
-
-            return False
-
-        if self.front == self.rear:
-
-            self.front, self.rear = -1, -1
-
-        else:
-
-            self.front = (self.front + 1) % self.size
-
-        return True
-
- 
-
-    def get_front(self) -> int:
-
-        if not self.is_empty():
-
-            return self.queue[self.front]
-
-        return -1
-
- 
-
-    def get_rear(self):
-
-        if not self.is_empty():
-
-            return self.queue[self.rear]
-
-        return -1
-
- 
-
-    def is_empty(self):
-
-        return self.front == -1
-
- 
-
-    def is_full(self):
-
-        return (self.front == 0 and self.rear == (self.size - 1)) or (self.front == (self.rear + 1) % self.size)
-
- 
-
-# Do not change the following code
-
-operations = []
-
-for specific_operation in input().split(','):
-
-    operations.append(specific_operation.strip())
-
-data = []
-
-for item in input().split(','):
-
-    item = item.strip()
-
-    if item == '-':
-
-        data.append([])
-
-    else:
-
-        data.append([int(item)])
-
-obj = MyCircularQueue(data[0][0])
-
-result = []
-
-for i in range(len(operations)):
-
-    if i == 0:
-
-        result.append(None)
-
-    elif operations[i] == "enqueue":
-
-        result.append(obj.enqueue(data[i][0]))
-
-    elif operations[i] == "get_rear":
-
-        result.append(obj.get_rear())
-
-    elif operations[i] == "get_front":
-
-        result.append(obj.get_front())
-
-    elif operations[i] == "dequeue":
-
-        result.append(obj.dequeue())
-
-    elif operations[i] == "is_full":
-
-        result.append(obj.is_full())
-
-    elif operations[i] == "is_empty":
-
-        result.append(obj.is_empty())
-
- 
-
-print(result)
-
-
-
+     
